e475cfb70e94f321221e9b8c66d471ab66448c8c
Roxangelica Rolan <rrolanbaez@gmail.com>
2024-03-26 16:55:05 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+// COSTIMICE FOR MY PROJECT
+
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
